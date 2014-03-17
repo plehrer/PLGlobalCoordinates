bd703a5b8283bfc5dc3ae9ec7f476c26d5038462
plehrer <plehrer@gmail.com>
2014-03-17 14:29:59 -0400

    Initial Commit

---
+++ PLGlobalCoordinates/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PLGlobalCoordinates
+//
+//  Created by Peter Lehrer on 3/17/14.
+//  Copyright (c) 2014 Peter Lehrer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
